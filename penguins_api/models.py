3bec601cf9266d9374607e174102d1467dc0684d
Caleb Hemphill <calebhemp@gmail.com>
2024-02-19 18:35:12 -0500

    Update model

--- penguins_api/models.py
+++ penguins_api/models.py
@@ -8,7 +8,7 @@ class Penguin(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
