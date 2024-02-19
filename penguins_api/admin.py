129e702ec1c5248887aa969e428de3c7fbb58abc
Caleb Hemphill <calebhemp@gmail.com>
2024-02-19 18:34:32 -0500

    Update model

--- penguins_api/admin.py
+++ penguins_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Penguin
+
+
+admin.site.register(Penguin)
--- penguins_api/models.py
+++ penguins_api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class Penguin(models.Model):
     # fields
-    user = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
